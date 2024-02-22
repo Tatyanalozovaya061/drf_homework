4e5f756a9194a9ac971a1853969dc50df1334086
tatyana lozovaya <tatka8632@gmail.com>
2024-02-22 08:06:25 +0300

    homework 24.1 доработки

--- config/urls.py
+++ config/urls.py
@@ -21,6 +21,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('users/', include('users.urls', namespace='users')),
     path('', include('materials.urls', namespace='materials')),
+    path('users/', include('users.urls', namespace='users')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
