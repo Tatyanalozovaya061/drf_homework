986d4e9664f76c1aa79ad2f96b9164b26d93881e
tatyana lozovaya <tatka8632@gmail.com>
2024-02-22 07:56:35 +0300

    homework 24.1 доработки

--- config/settings.py
+++ config/settings.py
@@ -23,7 +23,7 @@ load_dotenv(BASE_DIR / '.env')
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-t%1ysq_ve0j4oun8!xx&=#wgwf&hqy5e!@b20e5ixks+c&=igl'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
