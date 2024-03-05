558a77e6600583c5469ef52c7232f2b1287b125d
tatyana lozovaya <tatka8632@gmail.com>
2024-03-05 15:00:45 +0300

    homework 24.1 доработки

--- users/management/commands/fill.py
+++ users/management/commands/fill.py
@@ -1,29 +1,7 @@
-from django.core.management import BaseCommand
-
-from users.models import Payment
+from django.core.management import BaseCommand, call_command
 
 
 class Command(BaseCommand):
-
+    """Команда для первоначального заполнения БД данными из json-файла"""
     def handle(self, *args, **options):
-        # Payment.objects.all().delete()
-
-        paymant_list = [{
-            'user': 'user1',
-            'pay_date': '2023-09-01',
-            'pay_course': 'course1',
-            'pay_amount': '99999.00',
-        },
-            {
-            'user': 'user2',
-            'pay_date': '2023-08-01',
-            'pay_lesson': 'lesson1',
-            'pay_amount': '999.00',
-        },
-        ]
-
-        payments = []
-        for payment_item in paymant_list:
-            payments.append(Payment(**payment_item))
-
-        Payment.objects.bulk_create(payments)
+        call_command('loaddata', 'data.json')
